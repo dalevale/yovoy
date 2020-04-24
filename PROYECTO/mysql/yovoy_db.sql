f5467a7488940f66a5ece6f7e303f77371b8b08b
dalevale <dalevale@ucm.es>
2020-04-25 01:00:04 +0200

    Update yovoy_db.sql
    Sql actualizado con nuevos usuarios y nueva columna para la tabla eventos.

--- PROYECTO/mysql/yovoy_db.sql
+++ PROYECTO/mysql/yovoy_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 24, 2020 at 07:47 PM
+-- Generation Time: Apr 25, 2020 at 12:59 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
@@ -35,6 +35,7 @@ CREATE TABLE `event` (
   `creation_date` date NOT NULL DEFAULT current_timestamp(),
   `event_date` date NOT NULL,
   `capacity` int(11) NOT NULL,
+  `current_attendees` int(11) NOT NULL,
   `location` varchar(30) DEFAULT NULL,
   `description` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -43,11 +44,12 @@ CREATE TABLE `event` (
 -- Dumping data for table `event`
 --
 
-INSERT INTO `event` (`event_id`, `name`, `creator`, `creation_date`, `event_date`, `capacity`, `location`, `description`) VALUES
-(2, 'Barra Libre', 1, '2020-03-30', '2020-03-30', 20, 'Madrid SOL', '¡Vamos a beber cerveza gratis!'),
-(3, 'RokEnRol', 1, '2020-03-30', '2020-04-01', 100, 'WiZink', '¡Una noche de Rock and Rol!'),
-(8, 'Unli rice', 1, '2020-04-24', '2020-04-30', 99, 'Gran Via, Madrid', 'Si te gusta mucho el arroz, ven a hincharte!'),
-(10, 'Primer 100 personas, 1 menu whopper gratis!', 1, '2020-04-24', '2020-04-22', 100, 'Burger King, Calle Princesa, M', 'Primer 100 personas, 1 menu whopper gratis!');
+INSERT INTO `event` (`event_id`, `name`, `creator`, `creation_date`, `event_date`, `capacity`, `current_attendees`, `location`, `description`) VALUES
+(2, 'Barra Libre', 1, '2020-03-30', '2020-03-30', 20, 1, 'Madrid SOL', '¡Vamos a beber cerveza gratis!'),
+(3, 'RokEnRol', 1, '2020-03-30', '2020-04-01', 100, 1, 'WiZink', '¡Una noche de Rock and Rol!'),
+(8, 'Unli rice', 1, '2020-04-24', '2020-04-30', 99, 0, 'Gran Via, Madrid', 'Si te gusta mucho el arroz, ven a hincharte!'),
+(10, 'Primer 100 personas, 1 menu whopper gratis!', 1, '2020-04-24', '2020-04-22', 100, 0, 'Burger King, Calle Princesa, M', 'Primer 100 personas, 1 menu whopper gratis!'),
+(11, 'Bingo exclusivo!', 4, '2020-04-24', '2020-06-05', 20, 0, 'Calle Manuela Malasaña, Madrid', 'Aqui es divertido! Podrás ganar premios que no puedes imaginar!');
 
 -- --------------------------------------------------------
 
@@ -68,7 +70,9 @@ INSERT INTO `event_tags` (`event_id`, `tag`) VALUES
 (8, 'arroz'),
 (10, 'bk'),
 (10, ' hamburgesa'),
-(10, ' burgerking');
+(10, ' burgerking'),
+(11, 'bingo'),
+(11, ' premio');
 
 -- --------------------------------------------------------
 
@@ -88,8 +92,11 @@ CREATE TABLE `join_event` (
 --
 
 INSERT INTO `join_event` (`event_id`, `user_id`, `join_date`, `accepted`) VALUES
-(2, 9, '0000-00-00', 0),
-(3, 9, '0000-00-00', 0);
+(2, 2, '0000-00-00', 1),
+(2, 3, '0000-00-00', 1),
+(2, 5, '0000-00-00', 1),
+(2, 6, '0000-00-00', 1),
+(3, 3, '0000-00-00', 1);
 
 -- --------------------------------------------------------
 
@@ -115,7 +122,10 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`user_id`, `email`, `password`, `username`, `name`, `img_name`, `creation_date`, `type`) VALUES
 (1, 'admin@yovoy.com', '$2y$10$hedED5zbm5TpTNTCizsURujEKUiA873d7Qj0HD8F/xtGesKM4g52m', 'eladmin', 'admin', 'admin.png', '2020-04-24', 0),
 (2, 'maria@yovoy.com', '$2y$10$C9h7umkfFpTVvvPQsHyFNubDrAv/rCbESjDemDOapg1Dbfx6RAo5.', 'megustaeventos', 'Maria', 'default.jpg', '2020-04-24', 1),
-(3, 'pablo@yovoy.com', '$2y$10$AgyNHL35/Iwl/EhgTD0kc.4i/0zJMpMf.NXlGkqOm/TKU9JHvbLFC', 'meaburro', 'Pablo', 'default.jpg', '2020-04-24', 1);
+(3, 'pablo@yovoy.com', '$2y$10$AgyNHL35/Iwl/EhgTD0kc.4i/0zJMpMf.NXlGkqOm/TKU9JHvbLFC', 'meaburro', 'Pablo', 'default.jpg', '2020-04-24', 1),
+(4, 'manuel123@yovoy.com', '$2y$10$IdGxa/yeRXlYiLjH1V5iZOT.1P8X5lv7lpr84fiIDW2rAFY3jrSvi', 'elcapitan', 'Manuel', 'default.jpg', '2020-04-24', 1),
+(5, 'isabel789@yovoy.com', '$2y$10$j1e5Oq8EUldBF0tWcRa9aeqlh8xp0blZLYxTen5Z3WZlFADBPqbAa', 'isadora', 'Isabel', 'default.jpg', '2020-04-24', 1),
+(6, 'ana12345@yovoy.com', '$2y$10$zQNBXXcyMdcVVDaqL6howOl4QeDj1fpXJBUaiAZ5SUdbKPd41Tyx6', 'anaanaana', 'Ana Velasquez', 'default.jpg', '2020-04-25', 1);
 
 --
 -- Indexes for dumped tables
@@ -155,13 +165,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `event`
 --
 ALTER TABLE `event`
-  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `user_id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
