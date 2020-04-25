79cb1d54568e95c8db0b2e9cb1228a013f01141c
potchi23 <53357059+potchi23@users.noreply.github.com>
2020-04-25 20:44:35 +0200

    Add files via upload

--- PROYECTO/eventItem.php
+++ PROYECTO/eventItem.php
@@ -6,15 +6,15 @@ require_once __DIR__.'/includes/config.php';
     $conn = $app->bdConnection(); 
     $eventDAO = new EventDAO($conn);
     $userDAO = new UserDAO($conn);
+    
+    if(!empty($_GET))
+        $_SESSION["event_id"] = $_GET["event_id"];
 
-    $eventId = $_GET["event_id"];
-
-    $event = $eventDAO->getEvent($eventId);
+    $event = $eventDAO->getEvent($_SESSION["event_id"]);
     $creatorId = $event->getCreator();
     
     $creator = $userDAO->getUser($creatorId);
-    $creatorUsername = $creator->getUsername();
-    $eventImgName = $event->getImgName();
+    $creatorName = $creator->getName();
 
     $creationDate = $event->getCreationDate();
     $eventDate = $event->getEventDate();
@@ -39,19 +39,16 @@ require_once __DIR__.'/includes/config.php';
 
     <div class = "tarjeta_naranja">
         <?php 
-            $eventImgDir = "includes/img/events/";
-            $eventImgPath = $eventImgDir . $eventImgName;
 
-            $currentUserId = isset($_SESSION["userId"]) ? $_SESSION["userId"] : null;
+            //$currentUserId = isset($_SESSION["userId"]) ? $_SESSION["userId"] : null;
             echo '<h1>'.$event->getName().'</h1>';
-            echo "<img src='" . $eventImgPath . "' alt='event' height='300' width='500'>";
-            echo '<p>'.'Creador: '.$creatorUsername.'</p>';
+            echo '<p>'.'Creador: '.$creatorName.'</p>';
             echo '<p>'.'Fecha de creación: '.$creationDate.'</p>';
             echo '<p>'.'Fecha del evento: '.$eventDate.'</p>';
             echo '<p>'.'Capacidad: '.$capacity.'</p>';
             echo '<p>'.'Lugar: '.$location.'</p>';
             echo '<p>'.'Descripción: '.$descripcion.'</p>';
-            $attendees = $eventDAO->getAttendees($eventId);
+            $attendees = $eventDAO->getAttendees($_SESSION["event_id"]);
             if(!count($attendees)==0){
                 echo '<p>En este evento también van:</p>';
                 for($i = 0; $i < count($attendees); $i++) {
@@ -65,18 +62,63 @@ require_once __DIR__.'/includes/config.php';
             if (isset($_SESSION["login"]) && $_SESSION["login"] = true){
                // echo "<input type="submit" value="Go to my link location" onclick="window.location='includes/joinEvent.php?event_id=".$eventId."';" />" 
                // echo '<input type="button" onclick=header('Location: includes/joinEvent.php?event_id=".$eventId."')>Me apunto!</input>';            
-               if($userDAO->isMyEvent($currentUserId, $eventId)){
-                   echo '<form method="POST" action="editEvent.php"><input type="hidden" name="eventId" value="'.$eventId.'"/>';
-                   echo '<input type="submit" value="Editar" name="Submit" id="frm1_submit" /></form>';
-			   }
-               else{
-                   echo '<form method="POST" action="includes/joinEvent.php"><input type="hidden" name="event_id" value="'.$eventId.'"/>';
-                   echo '<input type="submit" value="Me apunto!" name="Submit" id="frm1_submit" /></form>';
-			   }
-             //echo '<input type="submit" value="Me apunto!" name="Submit" id="frm1_submit" /></form>';
-			}
+               echo '<form method="POST" action="includes/joinEvent.php"><input type="hidden" name="event_id" value="'.$_SESSION["event_id"].'">';
+               echo '<input type="image" alt="submit" src="includes/img/boton_UNIRSE_1.png" title="Me apunto!" name="Submit" id="frm1_submit" /></form>';
+               //echo '<input type="submit" value="Me apunto!" name="Submit" id="frm1_submit" /></form>';
+            }
         ?>   
     </div>
+        
+    <div class = "tarjeta_naranja">
+        <?php
+            $form = new CommentsForm;
+            $form->manage();
+        ?>
+    </div>
+    
+    <div class = "tarjeta_naranja">
+        <?
+            $app = es\ucm\fdi\aw\Application::getSingleton();
+		    $conn = $app->bdConnection(); 
+            //mostrar eventos de BBDD
+            $userDAO = new UserDAO($conn);
+            $commentsDAO = new CommentsDAO($conn);
+            $commentList = $commentsDAO->getComments($_SESSION["event_id"]);
+            echo "<label>COMENTARIOS</label>";
+
+            if(sizeof($commentList) == 0){
+                echo '<div class="tarjeta_blanca">';
+                echo 'Parece que aún no hay comentarios...';
+                echo '</div>';
+            }
+            else{
+                while(sizeof($commentList) > 0){
+                    $comment = array_pop($commentList);
+                    
+                    $username = $userDAO->getUser($comment->getUserID())->getUsername();
+                    $ownerId = $userDAO->getUser($comment->getUserID())->getUserId();
+
+                    $_SESSION["comment_id"] = $comment->getID();
+
+                    echo '<div class="tarjeta_gris">';
+                    echo "Comentario de " .$username. " el ".$comment->getDate(). "</br>";
+
+                    echo '<div class="tarjeta_blanca">';
+                    echo $comment->getComment();
+                    echo '</div>';
+                    
+
+                    if(isset($_SESSION["userId"]) && ($ownerId == $_SESSION["userId"])){
+                        $form = new DeleteCommentForm;
+                        $form->manage();
+                    }
+                
+                    echo "</div>";
+                }
+            }
+        ?>
+    </div>
+
 
     <footer>
         <?php include 'includes/comun/pie.php' ?>
