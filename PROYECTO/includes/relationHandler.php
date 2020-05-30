a1916b3126b7e9a1e001bcd5671a741cf17ce436
dalevale <dalevale@ucm.es>
2020-05-30 23:18:26 +0200

    Cambios para el funcionamiento en VPS
    YA ESTA TODO. JOLIN.

--- PROYECTO/includes/relationHandler.php
+++ PROYECTO/includes/relationHandler.php
@@ -10,17 +10,23 @@ require_once __DIR__.'/config.php';
         $u1 = $_POST["userId"];
         $u2 = $_POST["profileId"];
         $script = '<script>$("#addFriendBtn").click(function () {
-                changeRelation($(this), "addFriend");
+                var ok = confirm("Estas seguro?");
+                if(ok)
+                    changeRelation($(this), "addFriend");
                 });
                 $("#blockUserBtn").click(function () {
-                changeRelation($(this), "blockUser");
+                var ok = confirm("Estas seguro?");
+                if(ok)
+                    changeRelation($(this), "blockUser");
                 });</script>';
         switch($task){
             case "addFriend":
                 $result = $userDAO->insertRelationship($u1, $u2, RelationManager::ADD, $u1);
                 $notificationsDAO->notify(NotificationsDAO::NEW_FRIEND_REQUEST,$u2, $u1, 'NULL');
                 $script = '<script>$("#cancelAddFriendBtn").click(function () {
-                changeRelation($(this), "cancelAddFriend");
+                var ok = confirm("Estas seguro?");
+                if(ok)
+                    changeRelation($(this), "cancelAddFriend");
                 });</script>';
             break;
             case "cancelAddFriend":
@@ -37,7 +43,9 @@ require_once __DIR__.'/config.php';
                 $activityDAO->addActivity($u1,ActivityDAO::USER, $u2, 'null', ActivityDAO::NEW_FRIEND);
                 $activityDAO->addActivity($u2,ActivityDAO::USER, $u1, 'null', ActivityDAO::NEW_FRIEND);
                 $script = '<script>$("#unfriendBtn").click(function () {
-                changeRelation($(this), "unfriend");
+                var ok = confirm("Estas seguro?");
+                if(ok)
+                    changeRelation($(this), "unfriend");
                 });</script>';
             break;
             case "unfriend":
@@ -46,7 +54,9 @@ require_once __DIR__.'/config.php';
             case "blockUser":
                 $result = $userDAO->insertRelationship($u1, $u2, RelationManager::BLOCK, $u1);
                 $script = '<script>$("#unblockUserBtn").click(function () {
-                changeRelation($(this), "unblockUser");
+                var ok = confirm("Estas seguro?");
+                if(ok)
+                    changeRelation($(this), "unblockUser");
                 });</script>';
             break;
             case "unblockUser":
